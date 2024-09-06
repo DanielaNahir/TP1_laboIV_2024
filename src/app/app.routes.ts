d7b79297b35094ad0134ac70000a866d4e5d9918
DanielaNahir <danielanahir927@gmail.com>
2024-09-06 12:05:49 -0300

    img login, inicio home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { Component } from '@angular/core';
+import { HomeComponent } from './page/home/home.component';
 
 export const routes: Routes = [
+    {
+        path:"",
+        redirectTo: "home",
+        pathMatch: 'full'
+    },
+    {
+        path:"",
+        component: HomeComponent
+    },
     {
         path: 'login',
         component: LoginComponent
