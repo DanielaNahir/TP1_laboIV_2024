c6dfc27da079274a6cd60953efa0b7ac5556940c
DanielaNahir <danielanahir927@gmail.com>
2024-09-20 09:23:31 -0300

    mensaje si el usuario ya se encuentra registrado

--- src/app/auth/registrarse/registrarse.component.ts
+++ src/app/auth/registrarse/registrarse.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registrarse',
@@ -15,10 +16,37 @@ export class RegistrarseComponent {
   clave = '';
 
   private router = inject(Router);
-
   private auth = inject(Auth);
+
   registro() {
-    createUserWithEmailAndPassword(this.auth, this.email, this.clave);
-    this.router.navigateByUrl("/home");
+      createUserWithEmailAndPassword(this.auth, this.email, this.clave).then(() => {
+        this.router.navigateByUrl("/home");
+      })
+      .catch((error) => {
+        this.handleError(error);
+        console.log("Error en la creación del usuario");
+      });;
+      
+    
+  }
+
+  handleError(error: any) {
+    if (error.code === 'auth/email-already-in-use') {
+      console.error('El correo electrónico ya está en uso.');
+      Swal.fire({
+        icon: 'error',
+        title: 'El correo electrónico ya está en uso',
+        text: 'El correo electrónico que intentas usar ya está registrado. Por favor, pruebe con otro o inicie sesion.',
+        heightAuto: false
+      });
+    } else {
+      console.error('Error durante la creación del usuario:', error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Ocurrió un error al intentar crear el usuario. Por favor, intenta nuevamente.',
+        heightAuto: false
+      });
+    }
   }
 }
