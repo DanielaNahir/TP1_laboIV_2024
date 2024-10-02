348626a6d1a0861ee5b366bb8f54354c0cd92e5e
DanielaNahir <danielanahir927@gmail.com>
2024-10-02 11:36:37 -0300

    img ahorcado

--- src/app/page/games/ahorcado/ahorcado.component.ts
+++ src/app/page/games/ahorcado/ahorcado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { and } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-ahorcado',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './ahorcado.component.css'
 })
 export class AhorcadoComponent {
+  palabra = "abdce";
+  teclas: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  teclasUsadas: string[] = [];
+  primerError = false;
+  cantErrores = 0;
 
+  clickTeclas(letra:string){
+    this.teclasUsadas.push(letra);
+    if(this.cantErrores > 0 && this.cantErrores < 7){
+      this.cantErrores++;
+    } else if(this.cantErrores == 0){
+      this.primerError = true;
+      this.cantErrores++;
+    }
+  }
 }
