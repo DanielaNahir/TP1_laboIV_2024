94a0927851476ba402296aed5c05ce0ddcf39c00
DanielaNahir <danielanahir927@gmail.com>
2024-10-09 13:46:00 -0300

    solo usuarios logeados pueden acceder a los juegos

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -6,37 +6,52 @@ import { DatabaseService } from '../../services/database.service';
 import { Mensaje } from '../../classes/mensaje';
 import { Subscription } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  
   user: User | null = null;
   private router = inject(Router);
   authServise = inject(AuthService);
-  protected firestore = inject(DatabaseService);
-  chat = false;
-
   protected authService = inject(AuthService);
-  protected mensajes: Mensaje[] = [];
-  subscribe: Subscription | null = null;
-  protected msjNuevo = "";
-  
+
   constructor(private auth: Auth) {}
 
   ngOnInit() {
-    this.auth.onAuthStateChanged(user => {
+    this.auth.onAuthStateChanged((user) => {
       this.user = user;
     });
   }
 
-  ngOnDestroy() {
-    this.subscribe?.unsubscribe();
+  clickJuegos(juego: string) {
+    if (this.user != null) {
+      switch (juego) {
+        case 'mayor-menor':
+          this.router.navigateByUrl("/games/mayor-menor");
+          break;
+        case 'snake':
+          this.router.navigateByUrl("/games/snake");
+          break;
+        case 'preguntados':
+          this.router.navigateByUrl("/games/preguntados");
+          break;
+        case 'ahorcado':
+          this.router.navigateByUrl("/games/ahorcado");
+          break;
+      }
+    } else {
+      Swal.fire({
+        title: 'Inicie sesion',
+        text: 'Solo los usuarios registrados pueden acceder a los juegos',
+        icon: 'error',
+        confirmButtonText: 'Cerrar'
+      });
+    }
   }
-
 }
